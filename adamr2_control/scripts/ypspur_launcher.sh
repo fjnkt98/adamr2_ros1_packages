32b70e6d8c74c268fc80a52a1482ce64e146e280
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2020-04-16 16:18:33 +0900

    Add: yp-spurを起動するシェルスクリプト

---
+++ adamr2_control/scripts/ypspur_launcher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Execute yp-spur in background
+# Argument 1 is the path of parameter file
+# Argument 2 is the device file of the motor driver
+ypspur-coordinator -p $1 -d $2 &
+echo "Robot parameters [$1] loaded."
+
+# If yp-spur is dead, reboot it.
+while true
+  do
+    isAlive=`ps -ef | grep " ypspur-coordinator " | \
+    grep -v grep | wc -l`
+
+    if [ $isAlive = 1 ]; then
+      : #pass
+    else
+      echo "ypspur restart"
+      ypspur-coordinator -p $1 -d /dev/sensors/icart-mini &
+    fi
+
+    sleep 1
+  done
