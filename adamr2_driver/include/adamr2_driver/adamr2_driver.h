d3917839a3fe07b1ccb0c647d3eae7492a8fa8b0
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2020-05-07 08:24:37 +0000

    Update: remove unnecessary include

--- adamr2_driver/include/adamr2_driver/adamr2_driver.h
+++ adamr2_driver/include/adamr2_driver/adamr2_driver.h
@@ -5,9 +5,6 @@
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
-#include <map>
-#include <string>
-#include <vector>
 
 namespace adamr2 {
   class Adamr2Driver : public hardware_interface::RobotHW {
