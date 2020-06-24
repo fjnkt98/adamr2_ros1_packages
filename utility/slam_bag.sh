a24d5d1aec0d4b53d7b637a4df28bc806405b40c
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2020-06-24 11:24:22 +0900

    Add: 途中

---
+++ utility/slam_bag.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+TODAY=$(date "+%Y%m%d")
+
+if [ ! -e ~/adamr2_experiments/simulation/slam/${TODAY} ]; then
+  mkdir -p ~/adamr2_experiments/simulation/slam/${TODAY}
+fi
+
+cd ~/adamr2_experiments/simulation/slam/${TODAY}
+DIR_NUM=$(ls -l | grep ^d | wc -l)
+TITLE_NUM=$((++DIR_NUM))
+
+mkdir exp$((TITLE_NUM))
+cd exp$((TITLE_NUM))
+
+mkdir bag
+cd bag
+
+rosbag record -O slam_bag /tf /tf_static /odom /scan __name:=slam_bagger
