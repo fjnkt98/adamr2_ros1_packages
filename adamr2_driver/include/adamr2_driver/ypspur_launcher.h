a80f4edffde0664fe91625bf80d3b9943e8b43bf
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2020-09-10 15:35:43 +0900

    Update: 関数名をいくつか変更，Cライブラリの名前空間を削除

--- adamr2_driver/include/adamr2_driver/ypspur_launcher.h
+++ adamr2_driver/include/adamr2_driver/ypspur_launcher.h
@@ -2,16 +2,6 @@
 #define ADAMR2_YPSPUR_LAUNCHER_H_
 
 #include <ros/ros.h>
-#include <ypspur.h>
-
-namespace unistd {
-  #include <unistd.h>
-} // namespace unistd
-
-namespace sys {
-  #include <sys/types.h>
-  #include <sys/wait.h>
-} // namespace sys
 
 namespace adamr2 {
   class YPSpurLauncher {
@@ -19,15 +9,15 @@ namespace adamr2 {
       YPSpurLauncher();
       ~YPSpurLauncher();
 
-    int launch();
-    int kill();
-    int relaunch();
-    bool isAlive();
+    void launch();
+    void terminate();
+    void relaunch();
+    bool processIsAlive();
 
     private:
-      unistd::pid_t pid_;
+      pid_t pid_;
       ros::NodeHandle pnh_;
-  }
+  };
 } // namespace adamr2
 
 #endif
