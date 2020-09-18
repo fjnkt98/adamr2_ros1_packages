0da839e1b7bda610768d0ec1882f8c5dd8557d4f
fjnkt98 <a19613@g.ichinoseki.ac.jp>
2020-09-18 13:43:22 +0900

    Add: 実機でSLAMのためのメッセージを記録するシェルスクリプト

---
+++ utility/bag_tool/slam_real.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+TODAY=$(date "+%Y%m%d")
+
+TARGET_DIR="~/adamr2_experiments/real/slam/${TODAY}"
+
+if [ ! e $TARGET_DIR ]; then
+  mkdir -p $TARGET_DIR
+fi
+
+cd $TARGET_DIR
+
+DIR_NUM=$(ls -l | grep ^d | wc -l)
+TITLE_NUM=$((++DIR_NUM))
+
+mkdir exp$((TITLE_NUM))
+cd exp$((TITLE_NUM))
+
+mkdir bag && cd bag
+
+rosbag record /tf \
+              /tf_static \
+              /adamr2/camera/depth/color/points \
+              /adamr2/camera/color/image_raw \
+              /adamr2/scan \
+              /adamr2/diff_drive_controller/odom \
